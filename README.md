c7e069daa4c2fb3216e8c2d10222f91a75250653
Angeleah Daidone <angeleah@gmail.com>
2014-01-20 16:35:42 -0600

    Add README.  Add license.

---
+++ README.md
@@ -0,0 +1,5 @@
+Photo Folder Maker
+==================
+This command line app creates a directory structure for me to organize my model photography based on a model name and their set name.  It was created to help me DRY up my import and sorting process after shoots.
+
+At this point, this app is hard coded to work with my set up so there is not much for you to do.  It takes a model name and a set name and creates folders based on those.
--- photo_folder_maker.rb
+++ photo_folder_maker.rb
@@ -5,29 +5,27 @@ ROOTDIR = "/Volumes/ang_1tb/1tbexternal_my_photography/#{model_name}/#{model_nam
 
 Dir.chdir("/Volumes/ang_1tb/1tbexternal_my_photography")
 
-def check_model(model_name) 
+def check_model(model_name)
    if Dir.exist?("/Volumes/ang_1tb/1tbexternal_my_photography/#{model_name}")
      puts "That model #{model_name} already has a file.  I will put the set inside the file."
-  else 
-    Dir.mkdir("/Volumes/ang_1tb/1tbexternal_my_photography/#{model_name}") 
+  else
+    Dir.mkdir("/Volumes/ang_1tb/1tbexternal_my_photography/#{model_name}")
     puts "That model #{model_name} did not exist so I created a file."
-  end     
+  end
 end
 
-
-def check_set(model_name,set_name) 
+def check_set(model_name,set_name)
      if Dir.exist?("/Volumes/ang_1tb/1tbexternal_my_photography/#{model_name}/#{model_name}_#{set_name}")
        puts "#{model_name} already has a set named that.  Please select another set name."
-    else 
-      Dir.mkdir("/Volumes/ang_1tb/1tbexternal_my_photography/#{model_name}/#{model_name}_#{set_name}") 
+    else
+      Dir.mkdir("/Volumes/ang_1tb/1tbexternal_my_photography/#{model_name}/#{model_name}_#{set_name}")
       puts "#{model_name}'s set was created."
-    end     
+    end
 end
 
 def populate_set(model_name,set_name)
   ["_originals","_first_favs","_to_edit","_high_res_tiff","_high_res_jpeg", "_web","_8x10"].each { |name| Dir.mkdir("#{ROOTDIR}#{name}")}
 end
-    
 
 check_model(ARGV[0])
 check_set(ARGV[0],ARGV[1])
